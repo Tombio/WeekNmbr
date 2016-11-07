2c1d8ef00d8e6d09d2225ae575ac754208d3abde
Tomi Lahtinen <tomi.lahtinen@solita.fi>
2016-11-07 10:26:51 +0200

    Initial Commit

---
+++ WeekNmbr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeekNmbr
+//
+//  Created by Tomi Lahtinen on 07/11/2016.
+//  Copyright © 2016 Tomi Lahtinen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WeekNmbrTests/WeekNmbrTests.swift
@@ -0,0 +1,36 @@
+//
+//  WeekNmbrTests.swift
+//  WeekNmbrTests
+//
+//  Created by Tomi Lahtinen on 07/11/2016.
+//  Copyright © 2016 Tomi Lahtinen. All rights reserved.
+//
+
+import XCTest
+@testable import WeekNmbr
+
+class WeekNmbrTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
